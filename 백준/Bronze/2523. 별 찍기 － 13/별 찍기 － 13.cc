c95650918792977c18adef60a78336377dae5d60
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-01-09 14:00:01 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <stack>
+#include <queue>
+using namespace std;
+
+string make_star(int n){
+    string star{""};
+    for(int i{0}; i < n; i++){
+        star += "*";
+    }
+    return star;
+} // 별 문자열 생성기
+
+int main(){
+    int n;
+    cin >> n;
+    
+    queue<string> que; // 오름차순으로 출력용
+    stack<string> stk; // 내림차순으로 출력용
+
+    for(int i{0}; i < n; i++){
+        que.push(make_star(i + 1));
+        stk.push(make_star(i + 1));
+    }
+
+    while(!que.empty()){
+        cout <<que.front() << '\n';
+        que.pop();
+    }
+
+    stk.pop(); // 개수가 n개일때가 겹치므로 제거
+    while(!stk.empty()){
+        cout << stk.top() << '\n';
+        stk.pop();
+    }
+    return 0;
+}
