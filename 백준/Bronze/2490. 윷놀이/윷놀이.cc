2b178a0cd76bbeaa648be6a5943c3114aedf9f93
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-01-12 14:34:33 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i{0}; i < 3; i++){
+        int sum{0};
+        for(int j{0}; j < 4; j++){
+            int n;
+            cin >> n;
+            sum += n;
+        }
+        switch(sum){
+            case 0:
+                cout << "D\n";
+                break;
+            case 1:
+                cout << "C\n";
+                break;
+            case 2:
+                cout << "B\n";
+                break;
+            case 3:
+                cout << "A\n";
+                break;
+            case 4:
+                cout << "E\n";
+        }
+    }
+
+    return 0;
+}
