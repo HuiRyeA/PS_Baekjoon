21fe5eedf416f61b1afbbe7b1a6a9416009a69ce
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-02-16 00:57:32 +0900

    [Silver IV] Title: 듣보잡, Time: 468 ms, Memory: 39856 KB -BaekjoonHub

---
+++ 백준/Silver IV/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string nh[500000];
+string mh[500000];
+int cn[500000];
+
+bool SortCompare(string a, string b)
+{
+    return a.compare(b) < 0;
+}
+
+int main(){
+    int n, m, cnt{0};
+    cin >> n >> m;
+    for(int i{0}; i < n; i++){
+        cin >> nh[i];
+    }
+    for(int i{0}; i < m; i++){
+        cin >> mh[i];
+    }
+    sort(nh, nh + n, SortCompare);
+    sort(mh, mh + n, SortCompare);
+    for(int i{0}; i < m; i++){
+        int left{0}, right{n - 1}, mid{n / 2};
+
+        while(left <= right){
+            int cur{nh[mid].compare(mh[i])};
+            if(cur == 0){
+                cn[cnt++] = i;
+                break;
+            } else if(cur < 0){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            mid = (left + right) / 2;
+        }
+    }
+    cout << cnt << endl;
+    for(int i{0}; i < cnt; i++){
+        cout << mh[cn[i]] << endl;
+    }
+
+    return 0;
+}
