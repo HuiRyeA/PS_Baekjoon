4707ced6d0cc75ab5e2590b2e80001057808ad63
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-02-18 00:24:03 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 4212 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -2,23 +2,20 @@
 #include <map>
 using namespace std;
 
+int n, m;
+string str1, str2;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-
-    int n, m;
     cin >> n >> m;
-
     map<string ,string> mp;
     for(int i{0}; i < n; i++){
-        string str1, str2;
         cin >> str1 >> str2;
-
         mp.insert(make_pair(str1, str2));
     }
 
     for(int i{0}; i < m; i++){
-        string str1;
         cin >> str1;
         cout << mp.find(str1)->second << endl;
     }
