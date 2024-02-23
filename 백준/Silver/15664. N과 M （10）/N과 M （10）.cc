23b0a29d22a890f550f5fe7367858e5c9b344e56
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-02-23 15:17:59 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, idx[8], base[8], visited[8], limits[8];
+
+void tracking(int cnt){
+    if(cnt == m){
+        for(int i{0}; i < cnt; i++){
+            cout << base[idx[i]] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i{0}; i < n; i++){
+        if(visited[i] >= limits[i]) continue;
+        if(cnt != 0 && idx[cnt - 1] > i) continue;
+        visited[i] += 1;
+        idx[cnt] = i;
+        tracking(cnt + 1);
+        visited[i] -= 1;
+    }
+
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i{0}; i < n; i++) cin >> base[i];
+    sort(base, base + n);
+
+    for(int i{0}; i < n; i++){
+        if(i == 0) limits[i] += 1;
+        else{
+            if(base[i] == base[i - 1]){
+                int baseN{i - 1};
+                for(baseN; limits[baseN] == -1 && baseN > -1; baseN--);
+                limits[baseN] += 1;
+                limits[i] = -1;
+            } else{
+                limits[i] += 1;
+            }
+        }
+    }
+
+    tracking(0);
+    return 0;
+}
