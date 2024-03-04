f3e21f275d070bd19b95218e34dca49034cfecbd
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-03-04 20:59:37 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i{1}; i < n; i++){
+        int temp = i;
+        int sum{i};
+        while(temp > 0){
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        if(sum == n) {cout << i << '\n'; return 0;}
+    }
+
+    cout << 0 << '\n';
+
+    return 0;
+}
